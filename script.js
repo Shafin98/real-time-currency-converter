9fe99f69cd286b735529ccd2d36e0b01be5f1aa7
Shafin98 <98sasiam@gmail.com>
2025-11-29 12:40:09 +0600

    Final push

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 function convert() {
 
     let amount = document.getElementById("amount").value;
-    console.log('amount', amount)
-
     let from = document.getElementById("from").value;
-
-    console.log('Form Data', from);
-
     let to = document.getElementById("to").value;
 
-    console.log('to Data', to);
 
     if (amount === "" || amount <= 0){
         alert("Please enter a valid amount");
