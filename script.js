813637b589a0ba2b5e660819a2f42c22940925a3
Shafin98 <98sasiam@gmail.com>
2025-11-29 12:11:35 +0600

    Can add amount and select currency

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ function convert() {
     let amount = document.getElementById("amount").value;
     console.log('amount', amount)
 
+    let from = document.getElementById("from").value;
+
+    console.log('Form Data', from);
+
 }
